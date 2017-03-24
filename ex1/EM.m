a8143b71b2f4b0721f2a754e3bb0dcf4fa99715d
Lukeeeeee <lukedong123@gmail.com>
2017-03-24 21:42:03 +0800

    update em still have bug

--- ex1/EM.m
+++ ex1/EM.m
@@ -11,9 +11,10 @@ load('Mask.mat');
 fprintf('opening file array_sample.mat \n\n');
 load('array_sample.mat')
 
-[m,n]=size(Mask) %get colums and rows
+[m,n]=size(Mask)%get colums and rows
 
 img=imread('309.bmp');
+
 
 img=img.*Mask;
 [a,b,c]=size(img)
@@ -22,63 +23,96 @@ subplot(1, 2, 1);
 imagesc(img); 
 title('Original');
 gray=rgb2gray(img);
+%subplot(1, 2, 2);
+%imagesc(gray); 
+%title('Gray');
+
+
+%gray=double(gray)/255.0;
 
 [m2,n2]=size(array_sample)
 graySample=zeros(m2,2);
 graySample(:,1)=array_sample(:,1);
 graySample(:,2)=array_sample(:,5);
 
-
 % EM algorithm
-
 mean1 = rand();
 standard_deviation1 = rand();
+a1 = 0.5;
+
 
 mean2 = rand();
 standard_deviation2 = rand();
+a2 = 0.5;
 
-label = zeros(size(array_sample, 1));
+
+label = zeros(size(array_sample), 1);
 i = 0;
+fflush(stdout);
 
-while(i<=10000)
+while(i<=50)
   % predict
-  if (mod(i, 100) == 0)
-    fprintf('runing epoch = i\n');
-   end
-  for i=1:m2
-    x=graySample(i,1);
-    px1=computeSingleGaussModule(x, label, 1, mean1, standard_deviation1 * standard_deviation1);
-    px2=computeSingleGaussModule(x, label, -1, mean2, standard_deviation2 * standard_deviation2);
+  if (mod(i, 5) == 0)
+      fprintf('runing epoch = %d\n', i);
+      countSampleError=0;
+      for j=1:m2
+        x=graySample(j,1);
+        px1=computeSingleGaussModule(x,mean1,standard_deviation1 * standard_deviation1);
+        px2=computeSingleGaussModule(x,mean2,standard_deviation2 * standard_deviation2);
+        countSampleError += countError(px1, px2, graySample(j,2));
+       endfor
+      fprintf("Error num = %d\n", countSampleError);
+      fflush(stdout);
+  end
+  for j=1:m2
+    x=graySample(j,1);
+    px1=computeSingleGaussModule(x, mean1, standard_deviation1 * standard_deviation1);
+    px2=computeSingleGaussModule(x, mean2, standard_deviation2 * standard_deviation2);
     if (px1 > px2)
-      label(i) = 1;
+      label(j) = 1;
      else
-      label(i) = -1;
+      label(j) = -1;
      end
-    [mean1, standard_deviation1] = computeSingleGaussGradient(graySample, label, +1, mean1, standard_deviation1);
-    [mean2, standard_deviation2] = computeSingleGaussGradient(graySample, label, -1, mean2, standard_deviation2);
-    i++;
-  endfor
+   endfor
+   
+  [mean1, standard_deviation1] = computeSingleGaussGradient(graySample, label, +1, mean1, standard_deviation1);
+  [mean2, standard_deviation2] = computeSingleGaussGradient(graySample, label, -1, mean2, standard_deviation2);
+  i++;
 end
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+R=img(:,:,1); %red  
+G=img(:,:,2); %green  
+B=img(:,:,3); %blue  
+
+tmpPixel=0;
+for i=1:a
+
+	for j=1:b
+		tmpPixel=double(gray(i,j));
+
+		if (tmpPixel!=0)
+			tmpPixel=double(tmpPixel)/255.0;
+			px1=computeSingleGaussModule(tmpPixel, mean1, standard_deviation1 * standard_deviation1);
+			px2=computeSingleGaussModule(tmpPixel, mean2, standard_deviation2 * standard_deviation2);
+			if (px1>px2)
+				R(i,j) = 255;  
+            	G(i,j) = 0;  
+            	B(i,j) = 0; 
+			else
+				R(i,j) = 255;  
+            	G(i,j) = 255;  
+            	B(i,j) = 0; 
+			end
+		end
+	end
+end
+resultRGB(:,:,1)=R(:,:);  
+resultRGB(:,:,2)=G(:,:);  
+resultRGB(:,:,3)=B(:,:);  
+
+subplot(1, 2, 2);
+imagesc(resultRGB); 
+title('resultRGB');
   
   
--- ex1/computeSingleGaussGradient.m
+++ ex1/computeSingleGaussGradient.m
@@ -1,4 +1,4 @@
-function [mean_new, standard_deviation_new] = computeSingleGaussGradient(X, y, label, mean, standard_deviation)
+function [mean_new, standard_deviation_new] = computeSingleGaussGradient(X, label, y, mean, standard_deviation)
   
   like_mean = 0.0;
   like_standard_deviation = 0.0;
@@ -6,17 +6,23 @@ function [mean_new, standard_deviation_new] = computeSingleGaussGradient(X, y, l
   count = 0;
   for i=1:m
     x = X(i, 1);
-    if(y==label)
+    if(y==label(i))
       like_mean += x;
       count ++;
      end
   endfor
-  mean = mean / count;
-  for i=1:m
-    x = X(i, 1);
-    like_standard_deviation += (x - mean) * (x - mean);
-   endfor
-  like_standard_deviation /= count;
-  
-  mean_new = like_mean;
-  standard_deviation_new = like_standard_deviation
+  if (count == 0)
+    mean_new = mean;
+    standard_deviation_new = standard_deviation;
+  else
+    like_mean = like_mean / count;
+    for i=1:m
+      x = X(i, 1);
+      if(y==label(i))
+        like_standard_deviation += (x - like_mean) * (x - like_mean);
+       end
+     endfor
+    like_standard_deviation /= count;
+    mean_new = like_mean;
+    standard_deviation_new = like_standard_deviation;
+    end
